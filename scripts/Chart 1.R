e15ad879c696a78b76ca4897aec90fa0f45ee337
Katherine Poch <kathypoch11@gmail.com>
2020-11-12 16:41:41 -0800

    done

--- scripts/Chart 1.R
+++ scripts/Chart 1.R
@@ -1,12 +1,9 @@
 mpvdata <- read.csv("https://raw.githubusercontent.com/info201b-au20/info201finalGEKK/gh-pages/2013-2019%20Killings%20by%20PD-Table%201.csv")
 library(tidyverse)
 #Order dataset by population size
-mpvdata <- mpvdata %>% arrange(desc(Total)) 
+mpvdata <- mpvdata %>% arrange(desc(Total))
 mpvdata1 <- mpvdata[-(103:106),] #take out N/A rows
-mpvdata2 <- mpvdata1[-(31:94),] #take out lowest population rows 
-
-
-
+mpvdata2 <- mpvdata1[-(31:94),] #take out lowest population rows
 
 
 #Chart 1: police homicide rate vs violent crime rate
@@ -20,18 +17,6 @@ mpvdata2_longer <- mpvdata2_longer[-(1:24),] #too many rows, delete
 
 ggplot(mpvdata2_longer,aes(City, value, fill = rate)) +
   geom_col(position ="dodge") +
-  coord_flip() + 
+  coord_flip() +
   labs(
     title = "Police Homicide Rate vs. Violent Crime Rate in Random Major U.S. Cities, 2013-2018")
-
-#Describe the purpose of including the chart:
-#The purpose of this chart is to see the comparison between average rates of violent 
-#crimes and average annual rates of police homicide in populated U.S. cities. 
-#This chart also reveals where there are high and low police homicide rates in the U.S. 
-
-#Describe any notable observations and insights from the chart:
-#Cities like Memphis and Washington have high violent crime rates, but low 
-#police homicide rates. Cities like Phoenix and Oklahoma City have high police homicide rates,
-#but low violent crime rates. 
-#This disparity of values reveals that the levels of violent crimes in the U.S. 
-#do not determine rates of police violence. 
--- scripts/summary.R
+++ scripts/summary.R
@@ -1,1 +1,37 @@
+mpvdata <- read.csv("https://raw.githubusercontent.com/info201b-au20/info201finalGEKK/gh-pages/2013-2019%20Killings%20by%20PD-Table%201.csv")
+library(tidyverse)
 
+summary_info <- list()
+summary_info$num_observations <- nrow(mpvdata) # number of rows
+summary_info$num_features <- ncol(mpvdata) # number of columns
+columns <- colnames(mpvdata)
+
+
+#What was the highest police homicide rate in a police department?
+summary_info$highest_homicide_rate <- mpvdata %>%
+  filter(Avg.Annual.Police.Homicide.Rate == max(Avg.Annual.Police.Homicide.Rate,
+                                                na.rm = T)) %>%
+  pull(Avg.Annual.Police.Homicide.Rate)
+
+#What was the highest police homicide rate for Black people in a police department?
+summary_info$highest_homicide_forblack <- mpvdata %>%
+  filter(Avg.Annual.Police.Homicide.Rate.for.Black.People ==
+           max(Avg.Annual.Police.Homicide.Rate.for.Black.People, na.rm = T)) %>%
+  pull(Avg.Annual.Police.Homicide.Rate.for.Black.People)
+
+#Which city has the highest violent crime rate?
+summary_info$highest_violent_rate <- mpvdata %>%
+  filter(Violent.Crime.Rate == max(Violent.Crime.Rate, na.rm = T)) %>%
+  pull(City)
+
+#Which city has the highest estimated average annual arrests?
+summary_info$highest_annual_arrests <- mpvdata %>%
+  filter(Estimated.Average.Arrests.per.Year 
+         == max(Estimated.Average.Arrests.per.Year,
+                                                   na.rm = T)) %>%
+  pull(City)
+
+#What is the nationwide average for police killings?
+summary_info$nation_police_killings <- mpvdata %>%
+  filter(City == "Nationwide Average") %>%
+  pull(All.People.Killed.by.Police..1.1.2013.12.31.2019.)
