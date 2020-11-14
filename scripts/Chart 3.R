8d77719f7e97e2ea8eff4e2e47136c4d00330e5d
eivy_cedeno <eivy1234@uw.edu>
2020-11-13 21:49:49 -0800

    message description

--- scripts/Chart 3.R
+++ scripts/Chart 3.R
@@ -1,8 +1,16 @@
 police_kill_df <- read.csv("https://raw.githubusercontent.com/info201b-au20/info201finalGEKK/gh-pages/2013-2020%20Police%20Killings-Table%201.csv")
 library(tidyverse)
 
+
+To be able to get a visual of the growth or decrease in the past decade, 
+we grabbed all the deaths in a given year and sorted it out. In the line graph
+below we can see that since 2013 there have been a consistent amount of deaths 
+by police reaching over 1000. We noticed that in 2020 there were the least 
+amount but take into consideration that the year is not over.
+
 # changes format of date so its easier to work with
-police_kill_df$Date.of.Incident..month.day.year. <- as.Date(police_kill_df$Date.of.Incident..month.day.year., '%m/%d/%y')
+police_kill_df$Date.of.Incident..month.day.year. <- as.Date(
+  police_kill_df$Date.of.Incident..month.day.year., '%m/%d/%y')
 
 # pulls the dates from the dataset
 # Grabs the years and gets rid of months and days
