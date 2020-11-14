8fabd556e638c7e07c2d09a873d5f0bc6ac94767
eivy_cedeno <eivy1234@uw.edu>
2020-11-13 20:24:49 -0800

    line graph complete

--- scripts/Chart 3.R
+++ scripts/Chart 3.R
@@ -1,23 +1,22 @@
 police_kill_df <- read.csv("https://raw.githubusercontent.com/info201b-au20/info201finalGEKK/gh-pages/2013-2020%20Police%20Killings-Table%201.csv")
 library(tidyverse)
-library(ggplot2)
-library(dplyr)
-police_kill_df$count <- 1
 
+# changes format of date so its easier to work with
 police_kill_df$Date.of.Incident..month.day.year. <- as.Date(police_kill_df$Date.of.Incident..month.day.year., '%m/%d/%y')
-deaths_per_year <- police_kill_df %>% group_by(Date.of.Incident..month.day.year.) %>% summarize(deaths = sum(count, na.rm <- T))
-#deaths_per_year <- deaths_per_year %>% drop_na()
-plot(deaths~Date.of.Incident..month.day.year.,data<-deaths_per_year)
 
-#ggplot(deaths_per_year, aes(x=Date.of.Incident..month.day.year.)) + stat_count(geom='line', aes(y=deaths))
-#ggplot(deaths_per_year, aes(x=Date.of.Incident..month.day.year.))
-
-#ggplot(deaths_per_year, aes(x = factor(Date.of.Incident..month.day.year.))) + geom_bar()
+# pulls the dates from the dataset
+# Grabs the years and gets rid of months and days
+# creates table with occuracnes per year
 dates <- police_kill_df$Date.of.Incident..month.day.year.
-#a <- table(x,format = "%Y")
 years <- (format(dates, format = "%Y"))
 deaths_per_year2<- as.data.frame(table(years))
-ggplot(deaths_per_year2, aes(x = factor(Freq))) + geom_bar()
-ggplot(deaths_per_year2, aes(x=years)) + stat_count(geom='line', aes(y=Freq))
 
-ggplot(deaths_per_year2,aes(years)) + geom_bar()
+# groups the year and deaths of that year as an array
+# plots the deaths per year in a line graph 
+a <- table(years)
+plot(a, 
+     type="o",
+     col = "blue",
+     main = "police brutality: YEAR vs. DEATHS", 
+     xlab = "Year",
+     ylab = "Deaths by Cops")
