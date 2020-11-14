f9e0b561e015ac1a623bfc0f589c1e5da88fd530
Kennett <kennett.vera@gmail.com>
2020-11-13 21:12:26 -0800

    Started work on the aggregated table..

--- scripts/table.R
+++ scripts/table.R
@@ -1,1 +1,11 @@
+df <- read.csv("https://raw.githubusercontent.com/info201b-au20/info201finalGEKK/gh-pages/2013-2020%20Police%20Killings-Table%201.csv")
+View(df)
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
 
+grouped <- df %>%
+  group_by(Victim.s.race)
+  Summarize(Victim.s.age)
+View(grouped)
+#pull(grouped)
